04fd363e3b2eff11a5deb18bcb453eef99510fa4
culee1999 <leecw0511@gmail.com>
2022-09-17 11:21:39 +0900

    divide 구현

--- plus.c
+++ plus.c
@@ -4,9 +4,24 @@ int add(int x, int y) {
 	return x + y;
 }
 
+int minus(int x, int y) {
+	return x - y;
+}
+
+int multiply(int x, int y) {
+	return x * y;
+}
+
+int divide(int x, int y) {
+	return x / y;
+}
+
 int main(void) {
 	int A = 10, B = 5;
-	printf("%d", add(A, B));
+	printf("%d\n", add(A, B));
+	printf("%d\n", minus(A, B));
+	printf("%d\n", multiply(A, B));
+	printf("%d\n", divide(A, B));
 
 	return 0;
 }
