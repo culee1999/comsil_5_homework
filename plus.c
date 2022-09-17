0851dacb69e61e556317af1b5f76b1924a9e85ab
culee1999 <108467258+culee1999@users.noreply.github.com>
2022-09-17 11:38:27 +0900

    Update plus.c

--- plus.c
+++ plus.c
@@ -16,20 +16,9 @@ int divide(int x, int y) {
 	return x / y;
 }
 
-
-
-
-
-
-
-
-
 int main(void) {
 	int A = 10, B = 5;
 	printf("%d\n", add(A, B));
-	printf("%d\n", minus(A, B));
-	printf("%d\n", multiply(A, B));
-	printf("%d\n", divide(A, B));
 
 	return 0;
 }
