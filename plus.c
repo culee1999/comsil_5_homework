8801a21a937aa5f037084760ca4f07dc4a67d4ec
culee1999 <108467258+culee1999@users.noreply.github.com>
2022-09-17 11:38:15 +0900

    Update plus.c

--- plus.c
+++ plus.c
@@ -12,23 +12,9 @@ int multiply(int x, int y) {
 	return x * y;
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
 int main(void) {
 	int A = 10, B = 5;
 	printf("%d\n", add(A, B));
-	printf("%d\n", minus(A, B));
-	printf("%d\n", multiply(A, B));
-
+	
 	return 0;
 }
