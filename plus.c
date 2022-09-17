436eaa94892b52e1f9e923c879f8a51368b49504
culee1999 <leecw0511@gmail.com>
2022-09-17 11:16:23 +0900

    minus 구현

--- plus.c
+++ plus.c
@@ -4,9 +4,14 @@ int add(int x, int y) {
 	return x + y;
 }
 
+int minus(int x, int y) {
+	return x - y;
+}
+
 int main(void) {
 	int A = 10, B = 5;
-	printf("%d", add(A, B));
+	printf("%d\n", add(A, B));
+	printf("%d\n", minus(A, B));
 
 	return 0;
 }
